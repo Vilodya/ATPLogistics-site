190a655b88275ff6aacf0bd93e36524d658cb348
Vilodya <rygyj95@gmail.com>
2025-07-28 18:51:35 +0500

    Scripts fixed

---
+++ scripts/sliders.js
@@ -0,0 +1,48 @@
+const partnersSwiper = new Swiper('.partners__slider-wrapper.swiper', {
+  slidesPerView: 5,
+  spaceBetween: 40,
+  loop: true,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  breakpoints: {
+    1200: {
+      slidesPerView: 5
+    },
+    992: {
+      slidesPerView: 4
+    },
+    768: {
+      slidesPerView: 3
+    },
+    576: {
+      slidesPerView: 2
+    },
+    0: {
+      slidesPerView: 1
+    }
+  }
+});
+
+  const deliverySwiper = new Swiper('.delivery__slider-wrapper.swiper', {
+    slidesPerView: 3,
+    spaceBetween: 32,
+    loop: true,
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    // responsive
+    breakpoints: {
+      1024: {
+        slidesPerView: 3
+      },
+      768: {
+        slidesPerView: 2
+      },
+      0: {
+        slidesPerView: 1
+      }
+    }
+  });
