54eb935de52e320cd2961017a1d75e8a709587da
Vilodya <rygyj95@gmail.com>
2025-08-11 16:08:37 +0500

    fix: edit modal

--- scripts/modal.js
+++ scripts/modal.js
@@ -1,5 +1,5 @@
 // Объявление переменных
-const cards = document.querySelectorAll('.card');
+const cards = document.querySelectorAll('.operator-benefits__grid-item');
 
 const modal = document.querySelector('.modal');
 const modalTitle = document.querySelector('.modal__title');
@@ -8,10 +8,10 @@ const modalText = document.querySelector('.modal__text');
 const detailsButton = document.querySelectorAll('.operator-benefits__btn');
 const closeButton = document.querySelector('.modal__close');
 
-// Функции
+// Открытие модалки
 document.querySelectorAll('.operator-benefits__btn').forEach(button => {
   button.addEventListener('click', (e) => {
-    const card = e.target.closest('.card'); // Получаем карточку-родителя
+    const card = e.target.closest('.operator-benefits__grid-item'); // Получаем карточку-родителя
     const title = card.getAttribute('data-title');
     const text = card.getAttribute('data-text');
 
@@ -22,7 +22,24 @@ document.querySelectorAll('.operator-benefits__btn').forEach(button => {
   });
 });
 
-// Закрытие модалки
-closeButton.addEventListener('click', () => {
+// Функция закрытия
+function closeModal() {
   modal.classList.remove('modal_is-opened');
+}
+
+// Закрытие по крестику
+closeButton.addEventListener('click', closeModal);
+
+// Закрытие при клике вне окна
+modal.addEventListener('click', (e) => {
+  if (e.target === modal) { // клик именно по фону
+    closeModal();
+  }
+});
+
+// Закрытие по Esc
+document.addEventListener('keydown', (e) => {
+  if (e.key === 'Escape') {
+    closeModal();
+  }
 });
